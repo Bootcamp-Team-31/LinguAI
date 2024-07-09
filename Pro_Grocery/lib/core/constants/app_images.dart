98c14921b21bc2e81f7f9948b70ee4b219127ba0
Zehrakturk <zehra.akturk15@gmail.com>
2024-07-09 17:57:19 +0300

    project edited

---
+++ Pro_Grocery/lib/core/constants/app_images.dart
@@ -0,0 +1,30 @@
+class AppImages {
+  static const roundedLogo = 'https://i.imgur.com/9EsY2t6.png';
+
+  /* <---- Homepage banner -----> */
+  static const homePageBanner = 'https://i.imgur.com/8hBIsS5.png';
+
+  /* <---- Image used on unknown page -----> */
+  static const illustrations404 = 'https://i.imgur.com/SGTzEiC.png';
+
+  /* <---- INTRO LOGIN BACKGROUND IMAGES -----> */
+  static const introBackground1 = 'https://i.imgur.com/YQ9twZx.png';
+  static const introBackground2 = 'https://i.imgur.com/3hgB1or.png';
+
+  /* <---- ONBOARDING IMAGES -----> */
+  static const onboarding1 = 'assets/images/onboarding1.png';
+  static const onboarding2 = 'assets/images/OIG1.png';
+  static const onboarding3 = 'assets/images/onboarding3.png';
+
+  /* <---- Other Illustrations -----> */
+  static const numberVerfication = 'https://i.imgur.com/tCCmY3I.png';
+  static const verified = 'https://i.imgur.com/vF1jB6b.png';
+
+  /* <---- Coupon Backgrounds -----> */
+  static const couponBackgrounds = [
+    'assets/images/coupon_background_1.png',
+    'assets/images/coupon_background_2.png',
+    'assets/images/coupon_background_3.png',
+    'assets/images/coupon_background_4.png',
+  ];
+}
